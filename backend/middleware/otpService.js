c80c55cc3973722d309f62fc92ab057f247b33b5
om945 <ombelekar21@gmail.com>
2025-10-04 17:57:26 +0530

    Refactor OTP service configuration to streamline email transport setup
    - Removed hardcoded SMTP host and port values for improved security and flexibility.
    - Continued use of environment variables for email credentials to maintain secure practices.

--- backend/middleware/otpService.js
+++ backend/middleware/otpService.js
@@ -3,12 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // true for 465, false for other ports
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER, // Use environment variable
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
