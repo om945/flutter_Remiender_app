6f73779dd93e3beb21d79a0367eeca616ae6c5ca
om945 <ombelekar21@gmail.com>
2025-10-04 17:50:53 +0530

    Refactor OTP service to use environment variables for email credentials
    - Added dotenv configuration to securely manage email user and password.
    - Updated nodemailer transport configuration to utilize environment variables instead of hardcoded values.

--- backend/middleware/otpService.js
+++ backend/middleware/otpService.js
@@ -1,12 +1,15 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false, // true for 465, false for other ports
+  service: 'gmail',
   auth: {
-    user: 'ombelekar21@gmail.com',
-    pass: 'qtro nnvl zbry zctw',
+    user: process.env.EMAIL_USER, // Use environment variable
+    pass: process.env.EMAIL_PASS,
   },
 });
 
